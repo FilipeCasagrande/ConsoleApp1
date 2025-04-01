41ffe401beedef0c3b7f437c59877c71aab1ae07
Filipe <Filipecasagrande1@gmail.com>
2025-04-01 19:14:23 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("jogo Corrida de Dados");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Precione ENTER para lançar o dado...");
+
+            Random dado = new Random();
+            
+        }
+    }
+}
